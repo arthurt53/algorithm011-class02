93baaa17c98a06b4fd390ee9354238b031be078b
arthurt53 <41412635+arthurt53@users.noreply.github.com>
2020-08-02 10:56:37 +0800

    Add files via upload

---
+++ Week_06/palindromic-substrings.py
@@ -0,0 +1,20 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        L = len(s)
+        cnt = 0
+        # 以某一个元素为中心的奇数长度的回文串的情况
+        for center in range(L):
+            left = right = center
+            while left >= 0 and right < L and s[left] == s[right]:
+                cnt += 1
+                left -= 1
+                right += 1
+        # 以某对元素为中心的偶数长度的回文串的情况
+        for left in range(L - 1):
+            right = left + 1
+            while left >= 0 and right < L and s[left] == s[right]:
+                cnt += 1
+                left -= 1
+                right += 1
+
+        return cnt
